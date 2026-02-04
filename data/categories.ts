b0d09422b954f7e4c42b480a41da97cc09af1e96
rdeano <rdpantino@gmail.com>
2026-02-04 15:44:53 +0800

    turned jfif file to jpg

--- data/categories.ts
+++ data/categories.ts
@@ -8,9 +8,9 @@ export const categories = [
       { name: 'Bulalo/Bone Marrow', price: 230,  image : '/images/bulalo.jpg', unit: 'kg' },
       { name: 'Shank', price: 290,  image : '/images/pierna.jpg', unit: 'kg' },
       { name: 'Lomo Head', price: 370,  image : '/images/lomohead.jpg', unit: 'kg' },
-      { name: 'Ox Feet', price: 160,  image : '/images/oxfeet.jfif', unit: 'kg' },
-      { name: 'Balbacua', price: 190,  image : '/images/balbacua.jfif', unit: 'kg' },
-      { name: 'Paklay', price: 130,  image : '/images/paklay.jfif', unit: 'kg' },
+      { name: 'Ox Feet', price: 160,  image : '/images/oxfeet.jpg', unit: 'kg' },
+      { name: 'Balbacua', price: 190,  image : '/images/balbacua.jpg', unit: 'kg' },
+      { name: 'Paklay', price: 130,  image : '/images/paklay.jpg', unit: 'kg' },
     ],
   },
   // {
